e392b676cdbc9b4a21964cc641cf28509c71fada
Christopher Metzger <chrismetzgr@gmail.com>
2018-05-10 16:20:26 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,7 @@
 def reverse_each_word(string)
   splitString = string.split(" ")
-  splitString.collect do |x|
+  newString = splitString.collect do |x|
   x.reverse
-end.join(" ") 
+end
+newString.join(" ")
 end

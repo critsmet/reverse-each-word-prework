904af3a7e6974bbb06cab5ade71caba55d0bc63b
Christopher Metzger <chrismetzgr@gmail.com>
2018-05-10 16:14:10 +0000

    automatically backed up by learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,10 @@
+def reverse_each_words(string)
+  splitString = string.split(" ")
+  splitString.collect do |x|
+  x.reverse
+end 
+end
+
+def reverse_each_word(string)
+  reverse_each_words(string).join(" ")
+end
